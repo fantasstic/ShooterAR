99d2293bad25767a3b155ccb801b68161b074dff
fantasstic <mrfantassticgame@gmail.com>
2022-09-14 14:46:45 +0300

    Initial commit

---
+++ Assets/Script/Enemy/Enemy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _deathEffect;
+
+    public event UnityAction<Enemy> Dying;
+    
+    public void Die()
+    {
+        Dying?.Invoke(this);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Script/Player/Shooter.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private Bullet _bulletTemplate;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_bulletTemplate, _shootPoint);
+        }
+    }
+
+}
